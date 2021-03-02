8cd4b5095193ca9847c537eb5a7acdefa6a9ca5d
João Holz <joao.holz+tegrus@tegrus.com.br>
2021-03-02 08:28:19 -0300

    timer working

--- utils/timer.py
+++ utils/timer.py
@@ -1,15 +1,19 @@
-import datetime as dt
+from datetime.datetime import now()
 
-class timer():
-	def __init__(self):
-		self.start = None
+class Timer():    
+    def __init__(self, start_now = False):
+        if start_now:
+            self.start()
 
-	def start(self):
-		self.start = dt.datetime.now()
-
-	def stop(self):
-		self.end = dt.datetime.now()
-		self.duration = self.end - self.start
-
-	def pretty_print():
-		print('Time taken: {}'.format(self.duration))
+    def start(self):
+        if hasattr(x,'start_time'):
+            print('Started at: {}'.format(self.start_time))
+        else:
+            self.start_time = dt.now()
+    
+    def stop(self):
+        if hasattr(x,'stop_time'):
+            print('Stopped at: {}'.format(self.stop_time))
+        else:
+            self.stop_time = dt.now()
+            self.duration = self.stop_time - self.start_time
