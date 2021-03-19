80a1cb401a6203574799d5356ef7283c580f48a0
João Holz <joao.holz+tegrus@tegrus.com.br>
2021-03-19 16:43:18 -0300

    my first lib in pypi

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='jh-utils',  
+     version='0.1',
+     author="João Holz",
+     email = 'joaopaulo.paivaholz@gmail.com',
+     description="Some simple functions to all projects",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/JohnHolz/jh_utils",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
