cd76c586833beb80ea21fd50e6da3018acc6a369
João Holz <joaopaulo.paivaholz@gmail.com>
2021-08-17 11:44:28 -0300

    automatic 0.1.7:

--- utils/utils.py
+++ utils/utils.py
@@ -3,3 +3,8 @@ import os, json
 def open_json(json_file):
     with open(json_file) as data:
         return json.load(data)
+
+def write_dotenv(env, file=".env"):
+    with open(".env", "w") as f:
+        for i in env:
+            f.write(f'{i}={env[i]}\n')
