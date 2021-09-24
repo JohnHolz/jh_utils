30545bd8172a464b9aff5b1682ea872a24d10932
João Holz <joaopaulo.paivaholz@gmail.com>
2021-09-24 12:14:22 -0300

    added plots

--- ml/time_series/plots.py
+++ ml/time_series/plots.py
@@ -1,18 +1,36 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def doble_line_plot(df, x_axis, column1, column2, xlim, ylabel = '', fontsize = 14):
-    fig, ax1 = plt.subplots(figsize=(15, 8))
-    ax2 = ax1.twinx()
-    ax1.set_ylim(xlim)
-    ax2.set_ylim(xlim)
+def two_line_plot(df, x1, x2, freq = 'M', figsize = (20,5), tittle = ''):
+    temp = df[['date_time',x1,x2]]
+    temp.index = temp.date_time
+    temp = temp.resample(freq).mean()
+    temp.reset_index(inplace=True)
+    plt.figure(figsize = figsize)
+    _ = sns.lineplot(x = temp['date_time'],y = temp[x1]).set_title(tittle,fontsize=20)
+    _ = sns.lineplot(x = temp['date_time'],y = temp[x2])
+    
+def two_hist_plot(df, x1, x2, freq = 'M', figsize = figsize2, tittle = ''):
+    temp = df[['date_time',x1,x2]]
+    temp.index = temp.date_time
+    temp = temp.resample(freq).mean()
+    temp.reset_index(inplace=True)
+    plt.figure(figsize = figsize)
+    _ = sns.histplot(df[x1],color = 'red').set_title(tittle,fontsize=20)
+    _ = sns.histplot(df[x2]) 
 
-    ax1.plot(df[x_axis], df[column2], color='red', lw=1)
-    ax2.plot(df[x_axis], df[column1], color='blue', lw=1)
+def one_line_plot(df, x1, freq = 'M', figsize = figsize2, tittle = ''):
+    temp = df[['date_time',x1]]
+    temp.index = temp.date_time
+    temp = temp.resample(freq).mean()
+    temp.reset_index(inplace=True)
+    plt.figure(figsize = figsize)
+    _ = sns.lineplot(x = temp['date_time'],y = temp[x1]).set_title(tittle,fontsize=20)
 
-    ax1.set_xlabel("Date")
-    ax1.set_ylabel(ylabel, fontsize=fontsize)
-    ax1.tick_params(axis="y")
-
-    ax2.set_ylabel(ylabel, fontsize=fontsize)
-    ax2.tick_params(axis="y")
+def one_hist_plot(df, x1, freq = 'M', figsize = figsize2, tittle = ''):
+    temp = df[['date_time',x1]]
+    temp.index = temp.date_time
+    temp = temp.resample(freq).mean()
+    temp.reset_index(inplace=True)
+    plt.figure(figsize = figsize)
+    _ = sns.histplot(df[x1],color = 'red').set_title(tittle,fontsize=20)
