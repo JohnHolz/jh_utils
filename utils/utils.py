e363cd9ccada30e8b8a5f773df896c932cf74f2b
João Holz <joaopaulo.paivaholz@gmail.com>
2021-10-14 11:24:12 -0300

    added print list and print dict + line to mensages

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 import os, json
+from numpy.random import randint
 
 def open_json(json_file):
     with open(json_file) as data:
@@ -13,13 +14,13 @@ def write_dotenv(env, file=".env"):
         for i in env:
             f.write(f'{i}={env[i]}\n')
 
-def to_print_dict(dic:dict):
+def to_print_dict(dic:dict,spaces = 4):
     """
     @ make the dict a key: value table string
     """
     ret = ''
     for i in dic:
-        ret = ret + f"""\n {i}: {dic[i]}"""
+        ret = ret + f"""\n{spaces*' '}{i}: {dic[i]}"""
     return ret
 
 def print_dict(dic:dict):
@@ -27,3 +28,21 @@ def print_dict(dic:dict):
     @ just print to_print_dict
     """
     print(to_print_dict(dic))
+
+def sample(vec):
+    """Return 1 random value of a vector 
+    Args:
+        vec :list: list or a pd.Series or pd.Dataframe
+    Returns:
+        what is inside the list/dataframe/series index []
+    """
+    return vec[randint(len(vec))]
+
+def to_print_list(ls):
+    ret = ''
+    for i in ls:
+        ret=ret+f'{i},'
+    return ret[:-1]
+
+def print_list(ls):
+    print(to_print_list(ls))
